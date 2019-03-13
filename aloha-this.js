0cc8816e9bbabadaf2368b20fe6343a9bed9c40a
mlilley <michael.lilley@gmail.com>
2019-03-12 18:33:10 -0700

    Update aloha-this.js

--- aloha-this.js
+++ aloha-this.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name         Aloha This!
+// @name         Aloha-this
 // @namespace    http://github.com/mlilley
 // @version      1.0
 // @description  Remove banner from aloha
@@ -7,10 +7,10 @@
 // @match        https://aloha--c.visualforce.com/apex/aloha_appLauncher*
 // @match        https://aloha--c.na52.visual.force.com/apex/aloha_appLauncher*
 // @require      http://ajax.googleapis.com/ajax/libs/jquery/1.5/jquery.min.js
-// @run-at       document-start
+// @grant        none
 // ==/UserScript==
+
 (function() {
-   'use strict';
-   $('div.aloha-carousel.slick-initialized.slick-slider').remove();
-   $('.tile').css("width: 150px; height: 80px");
+    'use strict';
+    $('div.aloha-carousel.slick-initialized.slick-slider').remove();
 })();
